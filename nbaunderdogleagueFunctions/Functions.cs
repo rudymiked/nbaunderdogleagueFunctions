7176865443c9f70f20487947d7d78d1e906403de
rudymiked <rudymiked@gmail.com>
2022-12-05 19:08:42 -0800

    adding UpdateGamesFromRapidAPI to update once a day

--- nbaunderdogleagueFunctions/Functions.cs
+++ nbaunderdogleagueFunctions/Functions.cs
@@ -35,5 +35,29 @@ namespace nbaunderdogleagueFunctions
                 msg = ex.Message;
             }
         }
+
+        [FunctionName("UpdateGamesFromRapidAPI")]
+        public static async Task UpdateGamesFromRapidAPI([TimerTrigger("0 0 0 * * *", RunOnStartup = false)] TimerInfo timer)
+        {
+            string msg;
+            try {
+
+                string api = "api/NBA/UpdateGamesFromRapidAPI/";
+
+                HttpClient httpClient = new();
+                HttpRequestMessage request = new() {
+                    Method = HttpMethod.Get,
+                    RequestUri = new Uri(AppConstants.NBAUnderdogLeagueAPIURL + api),
+                };
+
+                HttpResponseMessage response = await httpClient.SendAsync(request).ConfigureAwait(false);
+
+                response.EnsureSuccessStatusCode();
+
+                string content = await response.Content.ReadAsStringAsync();
+            } catch (Exception ex) {
+                msg = ex.Message;
+            }
+        }
     }
 }
